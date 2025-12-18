af59f4acfdd144a65286fb8cfc18ef8f663a886d
Daniil <89221449094dg@gmail.com>
2025-12-19 00:23:13 +0500

    Проверка доступа к админке

--- back/app/auth/user.py
+++ back/app/auth/user.py
@@ -5,20 +5,38 @@ from app.auth.tokens import decode_token
 from app.entities.base import User
 
 
-async def get_current_user(
-        request: Request,
-        session: AsyncSession,
-) -> User | None:
-    auth = request.headers.get("Authorization")
-    if not auth or not auth.startswith("Bearer "):
-        return None
+class CurrentUser:
+    def __init__(self, request: Request, session: AsyncSession):
+        self.request = request
+        self.session = session
+        self.user = None
+        self.checked = False
 
-    token = auth.split(" ")[1]
+    async def get(self) -> User | None:
+        if self.checked:
+            return self.user
 
-    try:
-        payload = decode_token(token)
-        user_id = int(payload["sub"])
-    except Exception:
-        return None
+        user = await self.__get_current_user()
 
-    return await session.get(User, user_id)
+        self.user = user
+        self.checked = True
+
+        return user
+
+    async def __get_current_user(self) -> User | None:
+        auth = self.request.headers.get("authorization")
+        if not auth or not auth.startswith("Bearer "):
+            return None
+
+        token = auth.split(" ")[1]
+
+        try:
+            payload = decode_token(token)
+            user_id = payload["sub"]
+        except Exception:
+            return None
+
+        return await self.session.get(User, user_id)
+
+async def get_current_user(request: Request, session: AsyncSession) -> CurrentUser:
+    return CurrentUser(request, session)
